30033f56d21e7ada7af7840d04a78bdaf95d1b2d
poncoe <poncoe4mac@gmail.com>
2019-08-06 00:46:52 +0700

    add detailActivity

---
+++ app/src/main/java/id/poncoe/latihandicoding/Java/recyclerview/DetailM.java
@@ -0,0 +1,49 @@
+package id.poncoe.latihandicoding.Java.recyclerview;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import id.poncoe.latihandicoding.R;
+
+public class DetailM extends AppCompatActivity {
+    TextView nameTxt, usernameTxt;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+
+        nameTxt = findViewById(R.id.nama_kucing);
+        usernameTxt = findViewById(R.id.isi);
+        img = findViewById(R.id.gambar);
+
+        //DAPETIN INTENT
+        Intent i = this.getIntent();
+
+        //MENERIMA DATA
+        String images = i.getExtras().getString("IMAGES_KEY");
+        String name = i.getExtras().getString("TITLE_KEY");
+        String username = i.getExtras().getString("ISI_KEY");
+
+        //BIND DATA
+        img.setImageURI(Uri.parse(images));
+        nameTxt.setText(name);
+        usernameTxt.setText(username);
+
+        Glide.with(this)
+                .load(images)
+                //.resize(1200, 800)                     // optional
+                .into(img);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
